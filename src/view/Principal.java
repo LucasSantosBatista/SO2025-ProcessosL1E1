517dc0fedc116e5de77f57401cbd99c690e2c8a1
LucasSantosBatista <142185716+LucasSantosBatista@users.noreply.github.com>
2025-03-09 11:14:14 -0300

    09/03/2025

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,12 +1,29 @@
 package view;
 
+import javax.swing.JOptionPane;
+
 import controller.RedesController;
 
 public class Principal {
 	public static void main(String[] args) {
 		RedesController control = new RedesController();
-		
-		control.ip();
-		control.ping();
+
+		while (true) {
+			String[] options = { "IP", "Ping", "Sair" };
+			int opt = JOptionPane.showOptionDialog(null, "Escolha um processo para executar: ", "Chamada de Processos",
+					JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+
+			switch (opt) {
+			case 0: 
+				control.ip();
+				break;
+			case 1: 
+				control.ping();
+				break;
+			case 2: 
+				System.exit(0);
+				break;
+			}
+		}
 	}
 }
