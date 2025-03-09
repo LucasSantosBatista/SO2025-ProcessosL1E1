d147330b43ff3de043b6c216aa62ec6014c743c9
LucasSantosBatista <142185716+LucasSantosBatista@users.noreply.github.com>
2025-03-09 10:45:12 -0300

    09/03/2025

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -1,29 +1,59 @@
 package controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class RedesController {
 	public RedesController() {
 		super();
 	}
-	
+
+	// Retorna sistema operacional
 	private String getOS() {
 		return System.getProperty("os.name");
 	}
-	
+
 	public void ip() {
-		if(getOS().contains("Windows")) {
-			try {
-				Process p = Runtime.getRuntime().exec("");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		// Inicia buffer para a escrita do processo
+		String process = "";
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("cmd /c ");
+
+		// Valida sistema operacional
+		if (getOS().contains("Windows")) {
+			process = "ipconfig";
+			buffer.append(process);
+
 		} else {
+			process = "ip addr";
+			buffer.append(process);
+		}
+
+		// Utiliza buffer.toString().split() para converter em String e vetor
+		try {
+			Process p = Runtime.getRuntime().exec(buffer.toString().split(" "));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			String line = "";
 			
+			if (getOS().contains("Windows")) { // No Windows: exibe ipv4 e adaptador
+				while ((line = reader.readLine()) != null) {
+					if (line.contains("IPv4") || line.contains("Adaptador"))
+						System.out.println(line + "\n");
+				}
+			} else { // No Linux: exibe número do adaptador e inet(ipv4)
+				while ((line = reader.readLine()) != null) {
+					if (line.contains("1:") || line.contains("2:") || line.contains("inet"))
+						System.out.println(line);
+				}
+			}
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
 		}
 	}
-	
+
 	public void ping() {
-		
+
 	}
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,5 +1,13 @@
 package view;
 
-public class Principal {
+import controller.RedesController;
 
+public class Principal {
+	public static void main(String[] args) {
+		RedesController control = new RedesController();
+		
+		
+		
+		control.ip();
+	}
 }
